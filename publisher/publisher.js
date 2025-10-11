3d184603a23d69d15cfea07070dcc587b0a138c0
Edwin A. Saavedra Navarro <edsaana@icloud.com>
2025-10-10 20:50:01 -0500

    Agregar archivo publisher_0.js con implementación de mensajes MQTT: unicast, multicast y broadcast.

--- publisher/publisher.js
+++ publisher/publisher.js
@@ -1,15 +1,30 @@
-const mqtt = require('mqtt');
-const client = mqtt.connect('mqtt://broker.hivemq.com');
+const mqtt = require("mqtt");
+const client = mqtt.connect("mqtt://broker.hivemq.com");
 
-client.on('connect', () => {
-  console.log('Conectado al broker MQTT');
+client.on("connect", () => {
+  console.log("Conectado al broker MQTT");
 
-  // UNICAST → mensaje a un usuario específico
-  client.publish('sistema/unicast/usuarioA', 'Mensaje privado a Usuario A');
+  // UNICAST
+  client.publish("sistema/unicast/usuarioA", "Mensaje privado a Usuario A", {
+    qos: 1,
+    retain: true,
+  });
 
-  // MULTICAST → mensaje a un grupo (ej: zona norte)
-  client.publish('sistema/multicast/zonaNorte', 'Actualización para Zona Norte');
+  // MULTICAST
+  client.publish(
+    "sistema/multicast/zonaNorte",
+    "Actualización para Zona Norte",
+    {
+      qos: 1,
+      retain: true,
+    },
+  );
 
-  // BROADCAST → alerta para todos los nodos
-  client.publish('sistema/broadcast/general', 'ALARMA GLOBAL: Servicio caído');
+  // BROADCAST
+  client.publish("sistema/broadcast/general", "ALARMA GLOBAL: Servicio caído", {
+    qos: 1,
+    retain: true,
+  });
+
+  console.log("Mensajes enviados con QoS 1 y retención activada");
 });
